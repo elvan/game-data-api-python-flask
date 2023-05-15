9e673761d73d9a0d6fb1eec4f8d769de6c5dd4d8
Elvan Hidayat <elvanhd@gmail.com>
2023-05-15 21:06:21 +0700

    remove dateformat

--- schemas.py
+++ schemas.py
@@ -2,9 +2,6 @@ from marshmallow import Schema, fields
 
 
 class GameSchema(Schema):
-    class Meta:
-        dateformat = '%d %M %Y'
-
     id = fields.Int(dump_only=True)
     date = fields.Date(required=True)
     team_1 = fields.Str(required=True)
